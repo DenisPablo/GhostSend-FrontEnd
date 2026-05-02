291b2a63763ecb0bc1a376a2fd8ef3128eb5ea78
DenisPablo <denispablo17@gmail.com>
2026-05-02 15:59:40 -0300

    docker-compose

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5257/api/v1/files';
+const BASE_URL = import.meta.env.VITE_API_URL || '/api/v1/files';
 
 export const api = {
     async uploadFile(fileBlob, maxDownloads, lifeTimeHours) {
